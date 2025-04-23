1c4a3728d16556220039e03fd85c5854c641d7e2
rodpcastro <rodrigopcastro018@gmail.com>
2025-04-22 23:13:28 -0300

    header template

---
+++ header.f90
@@ -0,0 +1,24 @@
+module module_name
+!
+! Purpose:
+!   Purpose.
+!
+! Author:
+!   Rodrigo Castro (GitHub: rodpcastro)
+!
+! Creation Date:
+!   22-04-2025
+!
+! Modification History:
+!   <dd-mm-yyyy> - <Name> - <Changes>
+!
+! References:
+!   Shanjie Zhang, Jianming Jin (1996). Computation of Special Functions.
+!
+  implicit none
+  private
+  public :: 
+  
+contains
+
+end module module_name
