bcad8f3d805cc7e8bd5e030b7ac4289c75d0efda
rodpcastro <rodrigopcastro018@gmail.com>
2025-04-23 01:28:53 -0300

    universal constants

---
+++ constants.f90
@@ -0,0 +1,27 @@
+module constants
+!
+! Purpose:
+!   Purpose.
+!
+! Author:
+!   Rodrigo Castro (GitHub: rodpcastro)
+!
+! Creation Date:
+!   23-04-2025
+!
+! Modification History:
+!   <dd-mm-yyyy> - <Name> - <Changes>
+!
+! References:
+!   Shanjie Zhang, Jianming Jin (1996). Computation of Special Functions.
+!
+  implicit none
+  private
+  public :: pi, gm
+  
+contains
+
+  pi = 3.141592653589793d0
+  gm = 0.5772156649015329d0 ! Euler's constant
+
+end module module_name
