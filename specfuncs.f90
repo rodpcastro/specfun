bde10f3c822bac656370e6aa4fb93953965a6f90
rodpcastro <rodrigopcastro018@gmail.com>
2025-04-23 01:30:35 -0300

    main program to test compilation and results

---
+++ specfuncs.f90
@@ -0,0 +1,28 @@
+program specfuncs
+!
+! Purpose:
+!   Purpose.
+!
+! Author:
+!   Rodrigo Castro (GitHub: rodpcastro)
+!
+! Creation Date:
+!   23-04-2025
+!
+! Modification History:
+!   <dd-mm-yyyy> - <Name> - <Changes>
+!
+! References:
+!   Shanjie Zhang, Jianming Jin (1996). Computation of Special Functions.
+!
+
+  use iso_fortran_env, only: real64
+  use expint, only: e1  
+
+  implicit none
+  
+  real(real64) :: x
+
+  print *, 'E1(', x, ') = ', e1(x)
+
+end program specfuncs
