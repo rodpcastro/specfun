ee5952ce0d122fa66e69e4a4f00689ef5c53a90c
rodpcastro <rodpcastro18@gmail.com>
2025-06-02 03:15:35 -0300

    updated test points setter

--- test/set_test_points.py
+++ test/set_test_points.py
@@ -13,39 +13,55 @@ mpmath.mp.dps = 16
 test_points_dir = Path(__file__).parent / 'test_points/'
 
 def set_test_points_exponential_integral_ei():
-    xv = np.logspace(-16, 2, 1000, dtype=np.float64)
+    # Ei(x) goes to infinity as x increases. Can't test it for x > 709.0.
+    xc = np.logspace(-100, -1, 100, dtype=np.float64)
+    xf = np.linspace(1.0, 709.0, 1000, dtype=np.float64)
+    xv = np.concatenate((xc, xf))
     yv = np.array([mpmath.ei(x) for x in xv])
     zv = np.column_stack((xv, yv))
     filepath = test_points_dir / 'exponential_integral_ei.csv'
     np.savetxt(filepath, zv, fmt='% .15e')
 
 def set_test_points_exponential_integral_e1x():
-    xv = np.logspace(-16, 2, 1000, dtype=np.float64)
+    # E1(x) goes to 0 as x goes to infinity. No need to test for x > 10³.
+    xc = np.logspace(-100, -1, 100, dtype=np.float64)
+    xf = np.linspace(1.0, 1000.0, 1000, dtype=np.float64)
+    xv = np.concatenate((xc, xf))
     yv = np.array([mpmath.e1(x) for x in xv])
     zv = np.column_stack((xv, yv))
     filepath = test_points_dir / 'exponential_integral_e1x.csv'
     np.savetxt(filepath, zv, fmt='% .15e')
 
 def set_test_points_exponential_integral_e1z():
-    px = np.linspace(1.0e-16, 100, 20, dtype=np.float64)
-    mx = -px[::-1].copy()
-    x = np.concatenate((mx, px))
+    # {z ∈ ℂ | Re(z) ≥ 0, z ≠ 0}
+    x1c = np.logspace(-100, -1, 25, dtype=np.float64)
+    x1f = np.linspace(1.0, 100.0, 25, dtype=np.float64)
+    x1w = np.logspace(3, 20, 10, dtype=np.float64)
+    x1 = np.concatenate(([0.0], x1c, x1f, x1w), dtype=np.float64)
+    y1c = np.logspace(-100, -1, 25, dtype=np.float64)
+    y1f = np.linspace(1.0, 100.0, 25, dtype=np.float64)
+    y1w = np.logspace(3, 20, 10, dtype=np.float64)
+    y1p = np.concatenate(([0.0], y1c, y1f, y1w), dtype=np.float64)
+    y1n = -y1p[::-1].copy()
+    y1 = np.concatenate((y1n, [0.0], y1p), dtype=np.float64)
+    U1, V1 = np.meshgrid(x1, y1)
+    W1 = U1 + 1j * V1
+    z1 = W1.ravel()
+    z1_mask = ~np.logical_and(z1.real == 0, z1.imag == 0)
+    z1 = z1[z1_mask]
 
-    py = np.linspace(1.0e-16, 100, 20, dtype=np.float64)
-    my = -py[::-1].copy()
-    y = np.concatenate((my, py))
+    # {z ∈ ℂ | -10² ≤ Re(z) < 0, 0.7 ≤ |Im(z)| ≤ 10²}
+    x2c = np.logspace(-100, -1, 25, dtype=np.float64)
+    x2f = np.linspace(1.0, 100, 25, dtype=np.float64)
+    x2 = -np.concatenate((x2c, x2f))[::-1]
+    y2p = np.linspace(0.7, 100, 50, dtype=np.float64)
+    y2n = -y2p[::-1].copy()
+    y2 = np.concatenate((y2n, y2p), dtype=np.float64)
+    U2, V2 = np.meshgrid(x2, y2)
+    W2 = U2 + 1j * V2
+    z2 = W2.ravel()
 
-    X, Y = np.meshgrid(x, y)
-    Z = X + 1j * Y
-    zv = Z.ravel()
-
-    # For {z ∈ ℂ | z ∈ (-61, 0]×(-0.6, 0.6)}, the implemented
-    # e1z cannot achieve the desired precision of 8 digits.
-    zre_mask = np.logical_and(zv.real > -61, zv.real <= 0)
-    zim_mask = np.abs(zv.imag) < 0.6
-    zmask = np.logical_and(zre_mask, zim_mask)
-    zv = zv[~zmask]
-
+    zv = np.concatenate((z1, z2))
     wv = np.array([mpmath.e1(z) for z in zv], dtype=np.complex128)
     uv = np.column_stack((zv, wv))
     filepath = test_points_dir / 'exponential_integral_e1z.csv'
